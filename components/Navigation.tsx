079f297c4390cb2a977ac2876aa32b0ba5d14c61
Joshua Oas <josh.oas@gmail.com>
2021-06-20 08:42:45 -0500

    added tailwind

---
+++ components/Navigation.tsx
@@ -0,0 +1,141 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { PlusIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'About', href: '/about', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navigation() {
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex">
+                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block lg:hidden h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
+                  <img
+                    className="hidden lg:block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    alt="Workflow"
+                  />
+                </div>
+
+
+
+
+                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                  {navigation.map((item) => (
+                    <Link key={item.name} href={item.href}>
+                    <a
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'px-3 py-2 rounded-md text-sm font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </a>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+                  >
+                    <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                    <span>New Request</span>
+                  </button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="pt-4 pb-3 border-t border-gray-700">
+              <div className="flex items-center px-5 sm:px-6">
+                <div className="flex-shrink-0">
+                  <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium text-white">{user.name}</div>
+                  <div className="text-sm font-medium text-gray-400">{user.email}</div>
+                </div>
+                <button className="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-3 px-2 space-y-1 sm:px-3">
+                {userNavigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
--- components/todos.tsx
+++ components/todos.tsx
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useStoreActions, useStoreState } from "../store/hooks";
 
+const people = [
+  {
+    name: 'Lindsay Walton',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80',
+  },
+  // More people...
+]
+const activityItems = [
+  { id: 1, person: people[0], project: 'Workcation', commit: '2d89f0c8', environment: 'production', time: '1h' },
+  // More items...
+]
+
 export default function Todos() {
   // Pull out state from our store
   const items = useStoreState(state => state.todos.items);
@@ -15,19 +28,66 @@ export default function Todos() {
   useEffect(() => setNewTodo(""), [items]);
 
   return (
-    <div>
-      <h2>Todo List</h2>
+    <div className="py-8">
+      <div className="max-w-lg mx-auto px-6">
+      <h2>Requests</h2>
+
+
+      <ul className="divide-y divide-gray-200">
+        {activityItems.map((activityItem) => (
+          <li key={activityItem.id} className="py-4">
+            <div className="flex space-x-3">
+              <img className="h-6 w-6 rounded-full" src={activityItem.person.imageUrl} alt="" />
+              <div className="flex-1 space-y-1">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm font-medium">{activityItem.person.name}</h3>
+                  <p className="text-sm text-gray-500">{activityItem.time}</p>
+                </div>
+                <p className="text-sm text-gray-500">
+                  Deployed {activityItem.project} ({activityItem.commit} in master) to {activityItem.environment}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+
       <ul>
         {items.map((todo, idx) => (
           <li key={idx}>{todo}</li>
         ))}
       </ul>
-      <input
-        type="text"
-        onChange={e => setNewTodo(e.target.value)}
-        value={newTodo}
-      />
-      <button onClick={() => add(newTodo)}>Add</button>
+
+      <div>
+      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+        Email
+      </label>
+      <div className="mt-1">
+        <input
+          type="text"
+          name="email"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="you@example.com"
+          aria-describedby="email-description"
+          onChange={e => setNewTodo(e.target.value)}
+          value={newTodo}
+        />
+      </div>
+      <p className="mt-2 text-sm text-gray-500" id="email-description">
+        We'll only use this for spam.
+      </p>
+    </div>
+    <button
+        onClick={() => add(newTodo)}
+        type="button"
+        className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Add
+      </button>
+      </div>
+
+
     </div>
   );
 }
